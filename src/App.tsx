7ca7526884a9d8f0e129048389c7b99861ca07b3
sagorranait <sagorranait@gmail.com>
2023-10-17 19:19:29 +0600

    Design of Header Section is done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
+import styled from "styled-components";
 
-function App() {
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Folders from "./components/Folders";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header/>
+      <Folders/>
+      <Footer/>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 1240px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 50px auto 0;
+  // height: 650px;
+  // background: #FFFFFF;
+  // border-radius: 10px;
+  // box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.5);
+`
---
+++ src/components/Header.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <Container>
+      <CommonArea>
+        <LogoImg src="./assets/logo.png" alt="Folder-Structure-Logo" />
+        <LogoText>Folder Structure Viewer</LogoText>
+      </CommonArea>
+      <CommonArea>
+        <UserText>sagorranait@gmail.com</UserText>
+        <UserImg src="https://avatars.githubusercontent.com/u/60606778?v=4" alt="Sagor Rana" />
+      </CommonArea>
+    </Container>
+  )
+}
+
+export default Header;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const CommonArea = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const LogoImg = styled.img`
+  width: 50px;
+`;
+
+const LogoText = styled.h1`
+  font-size: 25px;
+  font-weight: bold;
+  color: #333;
+`;
+
+const UserImg = styled.img`
+  width: 50px;
+  border-radius: 50px;
+  object-fit: cover;
+  border: 2px solid #e70844;
+  object-position: center center;
+`;
+
+const UserText = styled.p`
+  color: #9d9d9d;
+`;
